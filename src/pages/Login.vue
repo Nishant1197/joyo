e47e1f6b45fb9529ccaaa7b30f7e1bcf3c00b5e2
Nishant1197 <nishant.chaddha@cilalabs.com>
2020-12-29 20:20:17 +0530

    first commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <q-page class="flex q-pa-md">
+     <q-card class="full-width">
+   <!-- We used tab panels for login and registration  -->
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="login" label="Login" />
+          <q-tab name="register" label="Register" />
+        
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="login">
+           <Loginmain :tab="tab"/>
+          </q-tab-panel>
+
+          <q-tab-panel name="register">
+                     <Loginmain :tab="tab"/>
+
+        </q-tab-panel>
+
+          
+        </q-tab-panels>
+      </q-card>
+  </q-page>
+</template>
+
+<script>
+import Loginmain from "/home/lenovo/joyo/src/pages/Loginmain.vue"
+export default {
+  data () {
+    return {
+      tab: 'login'
+    }
+  },
+  components:{
+    Loginmain
+  }
+}
+
+</script>
---
+++ src/pages/Loginmain.vue
@@ -0,0 +1,55 @@
+<template>
+<q-form >
+          <q-input
+          @keyup.enter="checkmail()"
+          v-if="tab=='register'"
+           class="q-mb-md" outlined v-model="form.name" 
+          label="Name" />
+          <q-inpu
+        
+           <q-input class="q-mb-md" outlined v-model="form.email" 
+          label="Email" />
+           <q-input outlined v-model="form.password" 
+          label="Password" />
+          <div >
+              <!-- q-space gives space horizontally -->
+              
+            <q-btn 
+        
+            color="primary"
+            class="q-mt-sm "
+            type="submit"
+             :label="tab" />
+          </div>
+</q-form>         
+
+</template>
+<script>
+export default {
+    name:'Loginmain',
+    props:['tab'],
+    data()
+    {
+        return{
+      form:
+          {
+              name:"",
+              email:"",
+              password:""
+          }
+      
+
+    }},
+    methods:
+    {
+        checkmail(){
+             if(this.form.name.length<5)
+        {
+            return "Invalid"
+        }
+        
+    }
+    
+    
+}}
+</script>
